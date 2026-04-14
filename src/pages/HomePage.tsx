f218eb453b5531975402157f6886264d87608bd4
moha998 <111455153+moha998@users.noreply.github.com>
2026-04-14 15:12:13 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,1 +1,9 @@
+import HeroSection from "../components/home/HeroSection";
 
+export default function HomePage() {
+  return (
+    <div className="bg-[#0B0E11] min-h-screen">
+      <HeroSection />
+    </div>
+  );
+}
