0c34deec8eddea16c3982de52e0d82766fdc6788
moha998 <111455153+moha998@users.noreply.github.com>
2026-04-15 13:52:34 +0300

    Delete src directory

--- src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import HeroSection from "../components/home/HeroSection";
-
-export default function HomePage() {
-  return (
-    <div className="bg-[#0B0E11] min-h-screen">
-      <HeroSection />
-    </div>
-  );
-}
